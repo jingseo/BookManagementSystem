a6528595dbc6ed732b116b694e57a9f67c1d55eb
jingseo <jinseo2@gnu.ac.kr>
2023-03-22 10:44:21 +0900

    commit

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+		
+		public static void main(String[] args) {
+			System.out.println("1.Add Book");
+			System.out.println("2.Edit Book");
+			System.out.println("3.View Book");
+			System.out.println("4.Delete Book");
+			System.out.println("5.Exit");
+		}
+		
+}
